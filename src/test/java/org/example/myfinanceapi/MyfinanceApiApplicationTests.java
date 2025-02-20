a04d79069e9ce6f93b5a306c03db56f2a9961827
pedro-henrique-teles <pedrohenriqueteles732@gmail.com>
2025-02-20 07:17:50 -0300

    first commit

---
+++ src/test/java/org/example/myfinanceapi/MyfinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myfinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
