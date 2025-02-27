d625b418ccb046f716c13926711e7d20b3120eb4
pedro-henrique-teles <pedrohenriqueteles732@gmail.com>
2025-02-27 11:05:17 -0300

    fix: Removido lombok da aplicação

--- src/main/java/org/example/myfinanceapi/model/entity/UserEntity.java
+++ src/main/java/org/example/myfinanceapi/model/entity/UserEntity.java
@@ -1,13 +1,7 @@
 package org.example.myfinanceapi.model.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @Entity(name = "User")
 public class UserEntity {
     @Id
@@ -19,4 +13,53 @@ public class UserEntity {
     @Column(unique = true)
     private String email;
     private String password;
+
+    public UserEntity(){}
+
+    public UserEntity(Long id, String name, String cpf, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
